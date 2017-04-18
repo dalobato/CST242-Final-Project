b814aed25af54bb07bf3ec4ff7bcbb7f2e7a2a0c
dalobato <dlobato95@icloud.com>
2017-04-18 14:53:17 -0400

    Update Author.java

--- Final Project/src/textbook/Author.java
+++ Final Project/src/textbook/Author.java
@@ -3,7 +3,7 @@ package textbook;
 /**
  * Created by dalob on 4/5/2017.
  */
-public class Author {
+public class Author implements Serializable {
     String authorFirstName;
     String authorLastName;
 
